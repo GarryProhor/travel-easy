d0099929ebb1701561abf36304f61ae68712ba51
GarryProhor <igorprohorchenko@gmail.com>
2022-09-26 11:54:31 +0300

    section slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.sliders').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow:"<button type='button' class='btn btn-slick-prev btn-slider'><img src='images/Group%20145.svg' alt='145'/></button>",
+        nextArrow:"<button type='button' class='btn btn-slick-next btn-slider'><img src='images/Group%20146.svg' alt='146'/></button>"
+    });
+});
