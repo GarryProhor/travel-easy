289cb94794ec50ad37072964aae3836c0f9f6fa8
GarryProhor <igorprohorchenko@gmail.com>
2022-09-26 15:22:19 +0300

    adaptive header

--- js/main.js
+++ js/main.js
@@ -7,4 +7,10 @@ $(document).ready(function () {
         prevArrow:"<button type='button' class='btn btn-slick-prev btn-slider'><img src='images/Group%20145.svg' alt='145'/></button>",
         nextArrow:"<button type='button' class='btn btn-slick-next btn-slider'><img src='images/Group%20146.svg' alt='146'/></button>"
     });
+
+    $('.mobile-menu').click(function (){
+        $('.mobile-menu span').toggleClass('active');
+        $('.menu').toggleClass('active');
+        $('body').toggleClass('owerlay');
+    });
 });
